49722d010f54ecf70894cc5508863639f69b2081
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-14 17:20:01 +0100

    add new image for jumbotron and fix card and css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,21 @@
-import negozio from "../assets/negozio-online.jpeg";
+import negozio from "../assets/imggg.jpg";
 
 export default function HomePage() {
     return (
         <>
             <main>
 
-                <div className="container">
-                    <img src={negozio} alt="Negozio Online" />
+                <div className="container d-flex flex-row align-items-center gap-5 p-5">
+                    <div>
+                        <img className="jumbotron" src={negozio} alt="Negozio Online" />
+                    </div>
+                    <div className="welcome">
+                        <h3>Benvenuti nel nostro negozio online!</h3>
+                        <p>Scopri una vasta gamma di prodotti di alta qualità a prezzi imbattibili. Offriamo offerte esclusive ogni settimana, quindi non perdere l'occasione di fare affari incredibili!</p>
+                    </div>
                 </div>
 
-            </main>
+            </main >
         </>
     )
 }
